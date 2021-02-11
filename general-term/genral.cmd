ca946d33eff9229f6a69c29e999e8c0b7a632697
YOUNIZAI <wgh2669279443@163.com>
2021-02-11 17:27:21 +0800

    new genral file

---
+++ general-term/genral.cmd
@@ -0,0 +1,10 @@
+- DHCP (Dynamic Host Configuration Protocol)
+```
+（动态主机配置协议）是一个局域网的网络协议。 指的是由服务器控制一段IP地址范围，客户机登录服务器时就可以自动获得服务器分配的IP地址和子网掩码
+```
+- RADIUS (Remote Authentication Dial In User Service)
+```
+RADIUS服务器：AA协议，通常用于网络访问、或流动IP服务，适用于局域网及漫游服务。 RADIUS协议包括RADIUS验证协议和RADIUS记账协议，分别定义于IETF RFC 2865和RFC 2866
+
+```
+
